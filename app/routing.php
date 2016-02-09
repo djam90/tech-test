bfc4fe2b14966db12d80735556a626d53919093a
Daniel Twigg <dtwigg1990@gmail.com>
2016-02-09 22:22:20 +0000

    Almost added controllers

--- app/routing.php
+++ app/routing.php
@@ -6,6 +6,8 @@ use Symfony\Component\Routing\Route;
 $routes = new RouteCollection();
 $routes->add('home', new Route('/'));
 $routes->add('people', new Route('/people'));
-$routes->add('people/edit', new Route('/people/edit'));
+$routes->add('people/edit', new Route('/people/edit', [
+    '_controller' => 'peopleEdit',
+]));
 
 return $routes;
--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 define('APP_PATH', dirname(dirname(__FILE__)) . '/');
+define('PEOPLE_FILENAME', APP_PATH . 'people.json');
+
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,34 +14,22 @@ use Symfony\Component\Routing\Matcher\UrlMatcher;
 $request = Request::createFromGlobals();
 $routes = include APP_PATH . 'app/routing.php';
 
-$peopleFilename = APP_PATH . 'people.json';
-
 // Common functions
 require_once(APP_PATH . 'functions.php');
 
 // Update the JSON data with the new data
 if (!empty($request->get('people'))) {
     $data = $request->get('people');
-    setPeopleFromFormInput($data, $peopleFilename);
+    setPeopleFromFormInput($data, PEOPLE_FILENAME);
 }
-
-// Get people from storage
-$people = getPeopleFromJsonFile($peopleFilename);
 
 $context = new RequestContext();
 $context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-
-    ob_start();
-
-    require_once(APP_PATH . 'app/pages/templates/header.php');
-    include sprintf(APP_PATH . 'app/pages/%s.php', $_route);
-    require_once(APP_PATH . 'app/pages/templates/footer.php');
-
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 
 } catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
@@ -50,3 +40,36 @@ try {
 $response->setStatusCode(200);
 $response->headers->set('Content-Type', 'text/html');
 $response->send();
+
+function render_template($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+
+    require_once(APP_PATH . 'app/pages/templates/header.php');
+    include sprintf(APP_PATH . 'app/pages/%s.php', $_route);
+    require_once(APP_PATH . 'app/pages/templates/footer.php');
+
+    return new Response(ob_get_clean());
+}
+
+function peopleEdit($request){
+
+    // Get people from storage
+    $people = getPeopleFromJsonFile(PEOPLE_FILENAME);
+
+    return render($request, compact('people'));
+}
+
+function render($request, $data){
+    extract($request->attributes->all(), EXTR_SKIP);
+    extract($data);
+
+    ob_start();
+
+    require_once(APP_PATH . 'app/pages/templates/header.php');
+    include sprintf(APP_PATH . 'app/pages/%s.php', $_route);
+    require_once(APP_PATH . 'app/pages/templates/footer.php');
+
+    return new Response(ob_get_clean());
+}
